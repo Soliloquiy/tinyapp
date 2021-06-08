1f1519a8356eb5e2d7073556e9b678a5c78622d9
Soliloquy <ibrahia@ucalgary.ca>
2021-06-08 00:08:40 +0000

    Create new ejs for posting user input and create new post event in server file for response

--- express_server.js
+++ express_server.js
@@ -1,13 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
-
+//body parser to make POST data readable
+//convert request body from buffer to readable string
+app.use(bodyParser.urlencoded({extended: true}));
+//set view engine to ejs
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 6)
+}
+
+console.log(generateRandomString())
+
 
 // app.get("/", (req, res) => {
 //   res.send("Hello!");
@@ -26,9 +37,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 //: indicates route paramater
 // will redirect to any resource that is inputted after :
-// resource value is stored in req.params in form "shortURL": ":shortURL"
+// resource value is stored in req.params in form "shortURL":"shortURL"
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
